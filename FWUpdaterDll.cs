4b16283f556c8eeee953545262b9ae3d3134202a
asylum128 <asylum128@gmail.com>
2021-01-06 14:41:12 -0500

    Initial version

---
+++ FWUpdaterDll.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace razer_mini_firmware_downgrader
+{
+    public class FWUpdaterDll
+    {
+        [DllImport("FWUpdaterDLL.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Auto)]
+        internal static extern IntPtr OpenDevice(uint VID, uint PID, ushort bcdpid, float blver, int reporttype, int featurelen, int inputlen, int outputlen);
+
+        [DllImport("FWUpdaterDLL.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        public static extern int EnterDeviceMode(IntPtr handle, byte mode);
+
+        [DllImport("FWUpdaterDLL.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Auto)]
+        public static extern void CloseDevice(IntPtr handle);
+
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace razer_mini_firmware_downgrader
+{
+    using System;    
+
+    internal static class Program
+    {
+        [STAThread]
+        private static void Main()
+        {
+            uint vid = 0x1532;
+            uint pid = 0x008A; // viper mini
+            int reportType = 4;
+            int featureReport = 91;
+            int inputReportLen = 0;
+            int outputReportLen = 0;
+
+            IntPtr handle = FWUpdaterDll.OpenDevice(vid, pid, 0, 0f, reportType, featureReport,inputReportLen, outputReportLen);
+            if(!(handle == IntPtr.Zero))
+            {
+                FWUpdaterDll.EnterDeviceMode(handle, 1);
+                FWUpdaterDll.CloseDevice(handle);
+                Console.WriteLine("Successful entered dev mode.");
+            }
+            else
+            {
+                Console.WriteLine("Failed to enter dev mode.");
+            }
+        }
+    }
+}
+
+
